a912c60da0898a967c838980e3088c57c4430380
LucasBehrend <lucasbehrend2006@gmail.com>
2023-07-10 08:23:59 -0300

    Revert "Revert "Merge pull request #1 from martinacenedese/main""
    This reverts commit 5a1a1da748f2f0034cf64aa3be7847e73571822e.

---
+++ Assets/Script/ObjetosManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosManager : MonoBehaviour
+{
+    public static bool tengoBoleadoras = false;
+    public static bool tengoGuitarra = false;
+    public static bool tengoCaballo = false;
+    public static bool tengoMate = false;
+    public static bool tengoBotella = false;
+    public static bool tengoPistola = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static bool TengoTodosLosObjetos()
+    {
+        if(tengoBoleadoras && tengoBotella && tengoCaballo && tengoGuitarra && tengoMate && tengoPistola)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
